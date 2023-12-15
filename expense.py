f01767006305c82d10f14be62672a9f8113e4238
Sophia Lawal <lawalsophia@gmail.com>
2023-12-15 16:23:10 +0100

    Add Documentation

--- expense.py
+++ expense.py
@@ -4,13 +4,32 @@ from datetime import datetime, timezone
 
 
 class Expense:
-    """_summary_"""
+    """
+    Expense class to represent an individual financial expense.
+    """
 
     def __init__(self, title: str, amount: float) -> None:
+        """
+        Args:
+            title (str): represents the title of an expense.
+            amount (float):  the amount of the expense.
+        """
         pass
 
-    def update(self, title: str = None, amount: float = None) -> str:
+    def update(self, title: str = None, amount: float = None) -> None:
+        """update the title and/or amount of the expense.
+        The updated_at attribute should be automatically set
+        to the current UTC timestamp whenever an update occurs.
+
+        Args:
+            title (str, optional): the title of the expense. Defaults to None.
+            amount (float, optional): the amount of the expense. Defaults to None.
+        """
         pass
 
     def to_dict(self) -> Dict:
+        """
+        Returns:
+            Dict: a dictionary representation of the expense.
+        """
         pass
--- expensedb.py
+++ expensedb.py
@@ -1,22 +1,55 @@
-from typing import Dict
+from typing import Dict, List
 from expense import Expense
 
 
 class ExpenseDatabase:
+    """
+    Manages a collection of Expense objects
+    """
+
     def __init__(self) -> None:
         pass
 
     def add_expense(self, expense: Expense) -> None:
+        """Adds an expense to the database.
+
+        Args:
+            expense (Expense): the expense to add to the database.
+        """
         pass
 
     def remove_expense(self, expense_id: str) -> None:
+        """Removes an expense from the database.
+
+        Args:
+            expense_id (str): A unique identifier for an expense.
+        """
         pass
 
     def get_expense_by_id(self, expense_id: str) -> None:
+        """Retrieves an expense from the database by its unique identifier.
+
+        Args:
+            expense_id (str): A unique identifier for an expense.
+        """
         pass
 
-    def get_expense_by_title(self, expense_title: str) -> None:
+    def get_expense_by_title(self, expense_title: str) -> List:
+        """Retrieves a list of expenses from the database by their title.
+
+        Args:
+            expense_title (str): The title of the expense.
+
+        Returns:
+            List: A list of expenses with the given title.
+        """
         pass
 
-    def to_dict(self) -> Dict:
+    def to_dict(self) -> List[Dict]:
+        """
+        Returns a list of dictionaries representing the expenses in the database.
+
+        Returns:
+            List[Dict]: A list of dictionaries representing the expenses in the database.
+        """
         pass
