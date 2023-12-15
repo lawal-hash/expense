5b752bebb89e1aece0e22d6a46992fe5f40c82dd
Sophia Lawal <lawalsophia@gmail.com>
2023-12-15 15:57:20 +0100

    Add expenses methods

---
+++ expense.py
@@ -0,0 +1,16 @@
+from typing import Dict, List
+import uuid
+from datetime import datetime, timezone
+
+
+class Expense:
+    """_summary_"""
+
+    def __init__(self, title: str, amount: float) -> None:
+        pass
+
+    def update(self, title: str = None, amount: float = None) -> str:
+        pass
+
+    def to_dict(self) -> Dict:
+        pass
